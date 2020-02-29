fed680c3da0d4fb40cd4af755cf1070b3c858091
BinBashBanana <51469593+BinBashBanana@users.noreply.github.com>
2020-02-28 22:41:12 -0800

    php-proxy-1

--- templates/url_form.php
+++ templates/url_form.php
@@ -5,35 +5,35 @@
 <style type="text/css">
 
 html body {
-	margin-top: 0px !important;
+	margin-top: 50px !important;
 }
 
 #top_form {
 	position: fixed;
 	top:0;
-	left:0;
+	text-align:center;
 	width: 100%;
-	
+
 	margin:0;
-	
+
 	z-index: 2100000000;
-	-moz-user-select: none; 
-	-khtml-user-select: none; 
-	-webkit-user-select: none; 
-	-o-user-select: none; 
-	
-	border-bottom:0px solid #CE00C0;
-	
-    background:#4D0243;
-	
-	height:0px;
-	line-height:0px;
+	-moz-user-select: none;
+	-khtml-user-select: none;
+	-webkit-user-select: none;
+	-o-user-select: none;
+
+	border-bottom:1px solid #151515;
+
+    background:gray;
+
+	height:37px;
+	line-height:35px;
 }
 
 #top_form input[name=url] {
-	width: 0px;
-	height: 0px;
-	padding: 0px;
+	width: 550px;
+	height: 20px;
+	padding: 5px;
 	font: 13px "Helvetica Neue",Helvetica,Arial,sans-serif;
 	border: 0px none;
 	background: none repeat scroll 0% 0% #FFF;
@@ -63,6 +63,18 @@ function smart_select(ele){
 </script>
 
 <div id="top_form">
+
+	<div style="width:800px; margin:0 auto;">
+	
+		<form method="post" action="index.php" target="_top" style="margin:0; padding:0;">
+			<input type="button" value="Home" onclick="window.location.href='local-2.html'">
+			<input type="text" name="url" value="<?php echo $url; ?>" autocomplete="off">
+			<input type="hidden" name="form" value="1">
+			<input type="submit" value="Go">
+		</form>
+		
+	</div>
+	
 </div>
 
 <script type="text/javascript">
